ca8df6f37b37ac16f4b4b9e4a41a37dc4724a526
MINECRAFTBUILDHAX1 <mmajcherczykolczak@gmail.com>
2026-01-09 21:58:56 +0000

    Update compare.js

--- compare.js
+++ compare.js
@@ -1,16 +1,24 @@
 document.querySelectorAll('.img-compare').forEach(container => {
   const overlay = container.querySelector('img:last-child');
+  let line = container;
 
-  container.addEventListener('mousemove', e => {
+  function moveSlider(x) {
     const rect = container.getBoundingClientRect();
-    let x = e.clientX - rect.left;
     x = Math.max(0, Math.min(x, rect.width));
     const percent = (x / rect.width) * 100;
 
     overlay.style.clipPath = `inset(0 0 0 ${percent}%)`;
+    container.style.setProperty('--slider-x', `${percent}%`);
+  }
+
+  container.addEventListener('mousemove', e => {
+    moveSlider(e.clientX - container.getBoundingClientRect().left);
   });
 
   container.addEventListener('mouseleave', () => {
-    overlay.style.clipPath = `inset(0 0 0 50%)`;
+    moveSlider(container.offsetWidth / 2);
   });
+
+  // Start centered
+  moveSlider(container.offsetWidth / 2);
 });
